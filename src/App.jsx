e9f426263c84056d9662e029e9fecbcb1b9c9e8d
Dani <drbrejnak@gmail.com>
2025-01-29 19:48:28 -0500

    Begin Project

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return(
+    <>
+      <header>
+        <h1>Puppy Bowl Roster 2025!</h1>
+      </header>
+      <div className="wrapper">
+        <main></main>
+        <form id="new-player-form"></form>
+      </div>
+    </>
+    )
+}
+
+export default App
